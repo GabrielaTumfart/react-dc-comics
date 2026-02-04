c5beb2bba16ca157043d6400a8fe21608c4d13d1
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-04 22:49:42 +0100

    aggiunto il big logo nello sfondo

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -12,18 +12,19 @@ export default function Footer() {
                     {/* COLUMNS */}
 
                         
-                    <div className="footer-columns"> {/* COLUMN 1 */}
+                    <div className="footer-columns"> 
+                        {/* COLUMN 1 */}
                             <div>
-                            <h3>DC COMICS</h3>
-                            <ul>
-                                <li>Characters</li>
-                                <li>Comics</li>
-                                <li>Movies</li>
-                                <li>TV</li>
-                                <li>Games</li>
-                                <li>Videos</li>
-                                <li>News</li>
-                            </ul>
+                                <h3>DC COMICS</h3>
+                                <ul>
+                                    <li>Characters</li>
+                                    <li>Comics</li>
+                                    <li>Movies</li>
+                                    <li>TV</li>
+                                    <li>Games</li>
+                                    <li>Videos</li>
+                                    <li>News</li>
+                                </ul>
 
                             <h3>SHOP</h3>
                             <ul>
@@ -34,41 +35,40 @@ export default function Footer() {
 
                             {/* COLUMN 2 */}
                             <div>
-                            <h3>DC</h3>
-                            <ul>
-                                <li>Terms Of Use</li>
-                                <li>Privacy Policy (New)</li>
-                                <li>Ad Choices</li>
-                                <li>Advertising</li>
-                                <li>Jobs</li>
-                                <li>Subscriptions</li>
-                                <li>Talent Workshops</li>
-                                <li>CPSC Certificates</li>
-                                <li>Ratings</li>
-                                <li>Shop Help</li>
-                                <li>Contact Us</li>
-                            </ul>
+                                <h3>DC</h3>
+                                <ul>
+                                    <li>Terms Of Use</li>
+                                    <li>Privacy Policy (New)</li>
+                                    <li>Ad Choices</li>
+                                    <li>Advertising</li>
+                                    <li>Jobs</li>
+                                    <li>Subscriptions</li>
+                                    <li>Talent Workshops</li>
+                                    <li>CPSC Certificates</li>
+                                    <li>Ratings</li>
+                                    <li>Shop Help</li>
+                                    <li>Contact Us</li>
+                                </ul>
                             </div>
 
                             {/* COLUMN 3 */}
                             <div>
-                            <h3>SITES</h3>
-                            <ul>
-                                <li>DC</li>
-                                <li>MAD Magazine</li>
-                                <li>DC Kids</li>
-                                <li>DC Universe</li>
-                                <li>DC Power Visa</li>
-                            </ul>
+                                <h3>SITES</h3>
+                                <ul>
+                                    <li>DC</li>
+                                    <li>MAD Magazine</li>
+                                    <li>DC Kids</li>
+                                    <li>DC Universe</li>
+                                    <li>DC Power Visa</li>
+                                </ul>
                             </div>
 
-                        
-
-                        
-
-
-
                     </div>
+
+                    {/* BIG LOGO BACKGROUND */}
+                        <div className="logo-bg">
+                            <img src="/src/assets/img/dc-logo-bg.png" alt="DC Logo" />
+                        </div>
 
 
                 </div>
