78f25594dac2001b41d91ab7281cbbbe8a519a6f
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-04 17:40:53 +0100

    strutturo il main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+export default function Main() {
+ 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  return <Main>questo è il Main</Main>
+    
+  
+}
+
