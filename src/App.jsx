5f8e2367f0f4bc25221b3c6c8fcf09bd41aeef33
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-04 17:25:13 +0100

    cerco l'errore

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/img/react.svg'
 import viteLogo from '/vite.svg'
 
 
 export default function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
       
-      <h1>Vite + React</h1>
+      <h1>VDC Comics (React Components)</h1>
       
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p> Lorem, ipsum dolor.
       </p>
     </>
   )
