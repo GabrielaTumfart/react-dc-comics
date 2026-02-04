7e7151706d9b5954a5885ed3df7a45056dcc887a
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-04 22:44:41 +0100

    aggiunte colonne del foot-colomms nella parte superiore del footer

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -5,9 +5,75 @@ export default function Footer() {
 
             {/* PARTE SUPERIORE */}
 
-            <div>
+            <div className="footer-top">
 
-                
+                <div className="container">
+
+                    {/* COLUMNS */}
+
+                        
+                    <div className="footer-columns"> {/* COLUMN 1 */}
+                            <div>
+                            <h3>DC COMICS</h3>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+
+                            <h3>SHOP</h3>
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                            </div>
+
+                            {/* COLUMN 2 */}
+                            <div>
+                            <h3>DC</h3>
+                            <ul>
+                                <li>Terms Of Use</li>
+                                <li>Privacy Policy (New)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                            </div>
+
+                            {/* COLUMN 3 */}
+                            <div>
+                            <h3>SITES</h3>
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                            </div>
+
+                        
+
+                        
+
+
+
+                    </div>
+
+
+                </div>
+
+
             </div>
 
 
