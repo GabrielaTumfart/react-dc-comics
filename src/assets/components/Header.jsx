7518b20323d2f50fcc8512723c41c9f2f09796f3
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-05 12:12:54 +0100

    ho creato l'array di headerLinks

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,20 +1,71 @@
 export default function Header() {
+
+    const headerLinks = [
+        { 
+            url: "#", 
+            text: "CHARACTERS", 
+            isActive: false },
+
+            { 
+                url: "#", 
+                text: "COMICS", 
+                isActive: false },
+
+                { 
+                    url: "#", 
+                    text: "MOVIES", 
+                    isActive: false },
+
+                    { 
+                        url: "#", 
+                        text: "TV", 
+                        isActive: false },
+
+                        { 
+                            url: "#", 
+                            text: "GAMES", 
+                            isActive: false },
+
+                            { 
+                                url: "#", 
+                                text: "COLLECTIBLES", 
+                                isActive: false },
+
+                                { 
+                                    url: "#", 
+                                    text: "VIDEOS", 
+                                    isActive: false },
+
+                                    { 
+                                        url: "#", 
+                                        text: "CFANS", 
+                                        isActive: false },
+
+                                        { 
+                                            url: "#", 
+                                            text: "NEWS", 
+                                            isActive: false },
+
+                                            { 
+                                                url: "#", 
+                                                text: "SHOP", 
+                                                isActive: false },
+
+            
+
+
+    ]
     return (
     
     <header>
         <img src="/src/assets/img/dc-logo.png" alt="DC Logo"/>
         <nav>
             <ul>
-                <li>CHARACTERS</li>
-                <li>COMICS</li>
-                <li>MOVIES</li>
-                <li>TV</li>
-                <li>GAMES</li>
-                <li>COLLECTIBLES</li>
-                <li>VIDEOS</li>
-                <li>FANS</li>
-                <li>NEWS</li>
-                <li>SHOP</li>
+                
+               
+                
+              
+                
             </ul>
         </nav>
 
--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -14,31 +14,32 @@ export default function Main() {
             <div className="container">
                 <ul>
                     <li>
-                        <img src="/src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <img src="/src/assets/img/buy-comics-digital-comics.png" alt="" />
                         <span>DIGITAL COMICS</span>
 
                     </li>
 
                     <li>
-                        <img src="/src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <img src="/src/assets/img/buy-comics-merchandise.png" alt=""/>
                         <span>DC MERCHANDISE</span>
 
                     </li>
 
                     <li>
-                        <img src="/src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <img src="/src/assets/img/buy-comics-subscriptions.png" alt="" />
                         <span>SUBSCRIPTION</span>
 
                     </li>
 
                     <li>
-                        <img src="/src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <img src="/src/assets/img/buy-comics-shop-locator.png" alt="" />
                         <span>COMIC SHOP LOCATOR</span>
 
                     </li>
 
                     <li>
-                        <img src="/src/assets/img/buy-comics-digital-comics.png" alt="" />
+                       
+                    <img src="/src/assets/img/buy-dc-power-visa.svg" alt="" />
                         <span>DC POWER VISA</span>
 
                     </li>
