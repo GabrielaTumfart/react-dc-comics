ba435c6c218d05ca5220ba05fad8771c108e8039
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-06 17:40:11 +0100

    creo lo scheletro del productList

--- src/assets/components/products/productList.jsx
+++ src/assets/components/products/productList.jsx
@@ -1,1 +1,19 @@
 import ProductCard from "./ProductCard";
+
+
+export default function ProductList(props) {
+
+    const cards = props.comics.map(function(comic, index) {
+        return <ProductCard key={index} comic={comic} />;
+
+
+
+
+    });
+
+    return (
+        
+        
+    
+    );
+}
