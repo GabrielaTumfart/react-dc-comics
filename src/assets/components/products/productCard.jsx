ac663cfe63729b91b6b8a018ebc7dbded802ef03
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-06 17:31:10 +0100

    uso il props che abbiamo imparato oggi alla lezione

--- src/assets/components/products/productCard.jsx
+++ src/assets/components/products/productCard.jsx
@@ -1,6 +1,8 @@
 export default function ProductCard(props) {
     return (
         <div className="card">
+            <img src={props.comic.thumb} alt={props.comic.series} />
+            <h4>{props.comic.series}</h4>
             
         </div>
         
